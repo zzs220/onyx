ebd848738b367e0fdada785c13fdc0369bcb7870
zrd <zrd@localhost.com>
2026-03-28 19:10:16 +0000

    add dnscrypt

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -6,13 +6,25 @@
     # dns = "systemd";
   };
 
-  services = {
-    # Enable NetworkManager dispatcher for scripts
-    # network-manager-applet.enable = true;
-  };
-  environment.systemPackages = with pkgs; [
-	  wireguard-tools wgcf
-  ];
+  # use dnscrypt-proxy
+    networking = {
+      useDHCP = false;
+      nameservers = [ "127.0.0.1" ];
+      networkmanager.dns = "none";
+      resolvconf.enable = true;
+    };
+
+    services.dnscrypt-proxy = {
+      enable = true;
+
+      settings = {
+        server_names = [
+        "cloudflare"
+        ];
+        listen_addresses = [ "127.0.0.1:53" ];
+        require_dnssec = true;
+      };
+    };
 
   services.openssh = {
     enable = true;
@@ -21,4 +33,8 @@
       PasswordAuthentication = false;
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    wireguard-tools wgcf
+  ];
 }
