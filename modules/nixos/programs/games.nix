7caadd16db2fee8bfdd39459fbe6ccf32a71af84
zrd <zrd@localhost.com>
2026-03-29 21:13:04 +0000

    added wine

--- modules/nixos/programs/games.nix
+++ modules/nixos/programs/games.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }:
-
 {
-    services = {
-        udev = {
-            packages = with pkgs; [
-                game-devices-udev-rules
-            ];
-        };
+  services = {
+    udev = {
+      packages = with pkgs; [
+        game-devices-udev-rules # controller compat
+      ];
     };
+  };
+  environment.systemPackages = with pkgs; [
+    wineWow64Packages.stagingFull
+    winetricks protontricks
+    heroic gamescope
+  ];
 }
