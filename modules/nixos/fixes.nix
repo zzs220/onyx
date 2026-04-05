8d2d312ce7ffea51dffe840fcaa4106e61389bf8
zrd <zrd@localhost.com>
2026-04-05 21:34:19 +0000

    reorg

--- modules/nixos/fixes.nix
+++ modules/nixos/fixes.nix
@@ -4,4 +4,20 @@
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc.lib
   ];
+  # appimage
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true; # execution attempt of .appimage file will be forced to use appimage-run some appimage may not work
+  programs.appimage.package = pkgs.appimage-run.override
+  {
+    extraPkgs = pkgs:
+      [
+        pkgs.icu
+        pkgs.libxcrypt-legacy
+        # pkgs.python312
+        # pkgs.python312Packages.torch
+      ];
+  };
+  environment.systemPackages = with pkgs; [
+    # steam-run
+  ];
 }
